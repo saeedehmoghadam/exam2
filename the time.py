6f8f29b3f475264bd46ccb5fb5962b5d287a9fb5
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-08-02 15:03:37 +0430

    Add files via upload

---
+++ the time.py
@@ -0,0 +1,24 @@
+import time
+
+
+
+hour = int(input("please enter an hour = "))
+minute = int(input("please enter a minute = "))
+second = int(input("please enter a second = "))
+
+
+
+h = (hour * 3600)
+m = (minute * 60)
+ 
+
+  
+print ( h + m + second )
+
+
+ 
+  
+
+
+
+   
