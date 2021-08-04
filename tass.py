dfd5fb9fd8f77da5518e3f2b69fb2663917d765e
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-08-04 15:16:40 +0430

    Add files via upload

---
+++ tass.py
@@ -0,0 +1,18 @@
+import random
+
+tas_number = random.randint(1,6)
+
+print ("tas_number:" , tas_number)
+
+
+if  tas_number == 6 :
+
+ print("very good")  
+
+ tas_number = random.randint(1,6)
+
+ print("tas_number:" , tas_number)
+
+else :
+      
+ print("check out the game")
