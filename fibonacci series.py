ca70dc386a81bbb4d490468ceee3cb747b1de003
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-09-05 19:48:20 +0430

    Update fibonacci series.py

--- fibonacci series.py
+++ fibonacci series.py
@@ -1,21 +1,17 @@
 import math
 
-x = int(input("please type the number : "))
+number = int (input(" Please type the number : "))
 
-l = []
+list=[]
 
-for i in range (x):
 
-       if i==0 or i==1 :
-           l.append(1)
-
+for i in range (number) :
+       if i==0 or i == 1 :
+               list.append(1)
        else :
-
-           fibonacci= l[i - 1] + l[i - 2 ]
-           l.append(fibonacci) 
-
-print(l)
-
+               fibo = list[i - 1] + list[i - 2]
+               list.append(fibo)       
+print(list) 
 
        
 
