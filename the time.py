c720f52bc54f0455116833de316ea4038f7af9e7
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-09-05 19:49:48 +0430

    Update the time.py

--- the time.py
+++ the time.py
@@ -1,20 +1,22 @@
+import math
 import time
+hour = int (input(" Please type the hour :"))
+minute = int (input(" Please type the minute :"))
+second = int (input( "Please type the second :"))
 
+while  hour > 12 or minute > 59 or second > 59 :
+        print ("no more than 12 hour or no more than 59 minute or no more than 59 second , please try again")
+        hour = int (input(" enter a oclock :"))
+        minute = int (input(" enter a minute :"))
+        second = int (input( "enter a second :"))
+    
 
+print (hour ," :" ,minute , " :" , second , )
+c = hour * 3600
+m= minute * 60
+s= c + m + second 
 
-hour = int(input("please enter an hour = "))
-minute = int(input("please enter a minute = "))
-second = int(input("please enter a second = "))
-
-
-
-h = (hour * 3600)
-m = (minute * 60)
- 
-
-  
-print ( h + m + second )
-
+print (s)
 
  
   
