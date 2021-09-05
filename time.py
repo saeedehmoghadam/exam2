962368087c6296c820246e19868a7e10828b79a9
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-09-05 19:50:35 +0430

    Update time.py

--- time.py
+++ time.py
@@ -4,8 +4,8 @@ import time
 s = int(input("please type the second : "))
 
 
-hour = ( s // 3600)
-minute = (s % 60) 
-second = ( minute - hour)
+hour =  s // 3600
+minute = s % 60
+second =  minute - hour
 
-print(hour , minute ,second )
+print(hour ,":", minute, ":",second )
